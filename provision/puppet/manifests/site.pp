c85891b3169607f3f1d215d23cb1f57509d53689
Thom Joy <thom.joy@massiveinteractive.com>
2014-12-22 16:53:52 +1100

    rename file

--- provision/puppet/manifests/site.pp
+++ provision/puppet/manifests/site.pp
@@ -20,13 +20,41 @@ file { '/var/www/':
   ensure => 'directory',
 }
 
-class { 'postgresql::globals':
-  manage_package_repo => true,
-  version             => '9.3',
+# Manage packages with apt (required on Debian/Ubuntu systems by
+# manage_package_repo option of postgresql::globals class)
+class { 'apt':
+  always_apt_update   => true,
 }
 
+$postgres_password = 'funkytown'
+
+# Forcibly activate the en_US.UTF-8 locale. Needed to have
+# UTF8 encoding in PostgreSQL databases.
+file { '/etc/default/locale':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  content => "LANG=en_US.UTF-8\n",
+} ->
+# Install PostgreSQL 9.4 server from the PGDG repository
+class { 'postgresql::globals':
+  version             => '9.4',
+  manage_package_repo => true,
+  encoding            => 'UTF8',
+  locale              => 'en_US.utf8',
+  # TODO: remove the next line after PostgreSQL 9.4 release
+  postgis_version     => '2.1',
+} ->
 class { 'postgresql::server':
-  postgres_password => 'funkytown'
+  listen_addresses           => '*',
+  ip_mask_deny_postgres_user => '0.0.0.0/32',
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  postgres_password          => $postgres_password,
+}
+
+# Install contrib modules
+class { 'postgresql::server::contrib':
+  package_ensure => 'present',
 }
 
 include apt-get::update
